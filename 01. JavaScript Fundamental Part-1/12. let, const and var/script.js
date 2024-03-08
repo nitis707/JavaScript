06d5a38b566d9444d762a73a8bfc0fb0054f6336
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-08 21:18:52 +0530

    Initial commit

---
+++ 01. JavaScript Fundamental Part-1/12. let, const and var/script.js
@@ -0,0 +1,21 @@
+/* Let and Const were introduced in ES6  */
+let age = 30;
+age = 31; // Reassigning value to a variable
+age = 32;
+
+/* If we use "const" to create a variable we cannot change its value like "let" */
+const birthYear = 2003; // It is used for constant
+/* birthYear = 2002; It will throw error */
+console.log(birthYear);
+
+/* We cannot create empty "const" variable. */
+// const job; it will throw error
+
+/* It is very old method to declare a variable at the current time we never need to use this. */
+var job = "Coder";
+job = "Teacher";
+console.log(job);
+
+/* We can simply create a variable without declare it. */
+withoutDeclareVariable = "35";
+console.log(withoutDeclareVariable);
---
+++ 01. JavaScript Fundamental Part-1/13. Basic Operator/script.js
@@ -0,0 +1,47 @@
+/* Arithmetic Operator */
+const now = 2024;
+const ageNitis = now - 2003;
+console.log("Age: " + ageNitis);
+
+const ageSarah = now - 2000;
+console.log("Age of Nitis: " + ageNitis, "\nAge of Sarah: " + ageSarah);
+
+/*
+2**3 means 2 to the power of 3 = 2*2*3
+*/
+console.log(ageNitis * 2, ageSarah / 10, 2**3);
+
+const firstName = "Nitish ";
+const lastName = "Singh";
+const concatenate = firstName + lastName;
+console.log(concatenate);
+
+
+
+/* Assignment Operator */
+let x = 10 + 5; // 15
+console.log("x: " + x);
+
+x += 10; // x = x + 10 = 25
+console.log("x += 10: " + x);
+
+x *= 4; // x = x*4 = 100
+console.log("x *= 4: " + x);
+
+x++; // x = x+1 = 101
+console.log("x++: " + x);
+
+x--; // x = x-1 = 100
+console.log("x--: " + x);
+
+
+
+/* Comparison Operator
+>
+<
+>=
+<=
+*/
+console.log("Age of Nitish is " + ageNitis + " greater than " + "Sarah " + ageSarah + " =", ageNitis > ageSarah);
+
+console.log(ageSarah >= 18);
