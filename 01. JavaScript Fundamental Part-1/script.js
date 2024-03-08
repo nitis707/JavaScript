7383971e4cac2067caf140c25929c23a2a31782e
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-08 15:25:05 +0530

    Initial commit

--- 01. JavaScript Fundamental Part-1/script.js
+++ 01. JavaScript Fundamental Part-1/script.js
@@ -1,4 +1,12 @@
+/*
+
 let js = "amazing";
 if (js === "amazing") {
   alert("JavaScript is Fun!");
 }
+
+*/
+
+// To display result on console
+let result = 40 + 8 + 23 - 10;
+console.log("Result = " + result);
