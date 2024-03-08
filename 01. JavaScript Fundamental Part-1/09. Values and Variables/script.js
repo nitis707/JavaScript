084f4d4f657588d508fffdd07cffcbb076b90de3
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-08 15:44:19 +0530

    Initial commit

---
+++ 01. JavaScript Fundamental Part-1/09. Values and Variables/script.js
@@ -0,0 +1,24 @@
+/* To show alert on the page
+let js = "amazing";
+if (js === "amazing") {
+  alert("JavaScript is Fun!");
+}
+*/
+
+let js = "amazing";
+if (js === "amazing") {
+  console.log("JavaScript is Fun!");
+}
+
+// To display result on console
+let result = 40 + 8 + 23 - 10;
+console.log("Result = " + result);
+
+/* Lecture - 9. Values and Variables */
+console.log("Nitish");
+console.log(21);
+
+let firstName = "Nitish";
+let lastName = "Singh";
+console.log(firstName + " " + lastName);
+
