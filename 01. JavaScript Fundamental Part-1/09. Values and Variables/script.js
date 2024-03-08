39bba0bd77389be9b5c89bf1fd34300dedf5e22d
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-08 15:55:00 +0530

    Initial commit

--- 01. JavaScript Fundamental Part-1/09. Values and Variables/script.js
+++ 01. JavaScript Fundamental Part-1/09. Values and Variables/script.js
@@ -22,3 +22,10 @@ let firstName = "Nitish";
 let lastName = "Singh";
 console.log(firstName + " " + lastName);
 
+/* In javaScript when we create a variable with multiple word it should be the first letter in LowerCase and we want to add another word then we can start with UpperCase letter like - firstName this is called CamelCase*/
+
+/* We can create variables in JavaScript like this also - first_name */
+let first_name = "Nitish";
+let first_name_person = "Nitish";
+
+/* We cannot create a variable start with a number like this 3years = 3; because it is illegal in JavaScript */
