0e5d0454a5555ec0f64aabf6f4dea78bbc987d27
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-10 17:11:47 +0530

    Initial commit

--- 01. JavaScript Fundamental Part-1/14. Operator Precedence/script.js
+++ 01. JavaScript Fundamental Part-1/14. Operator Precedence/script.js
@@ -1,5 +1,12 @@
 const now = 2037;
-const ageNitis = 2003;
-const agePriya = 2000;
+const ageNitis = now - 2003;
+const agePriya = now - 2000;
 
 console.log(now - 2000 > now - 2003);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageNitis + agePriya) / 2;
+console.log(ageNitis, agePriya, averageAge);
