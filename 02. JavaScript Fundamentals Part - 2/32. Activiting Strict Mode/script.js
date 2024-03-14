a2763dd8080f7fa6ad1c9307509c881b3b73fd00
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-14 10:08:23 +0530

    Initial commit

---
+++ 02. JavaScript Fundamentals Part - 2/32. Activiting Strict Mode/script.js
@@ -0,0 +1,7 @@
+/* 
+
+JavaScript's strict mode is a way to opt in to a restricted variant of JavaScript, thereby implicitly opting-out of "sloppy mode". Strict mode isn't just a subset: it intentionally has different semantics from normal code. Browsers not supporting strict mode will run strict mode code with different behavior from browsers that do, so don't rely on strict mode without feature-testing for support for the relevant aspects of strict mode. Strict mode code and non-strict mode code can coexist, so scripts can opt into strict mode incrementally.
+
+*/
+
+"use strict";
