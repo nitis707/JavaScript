058a40e33927585547fef0fe4dcf700d75dc11ba
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-03-24 10:25:29 +0530

    Initial commit

---
+++ 05. JavaScript in the Browser - DOM and Events Fundamentals/71. What's the DOM and DOM Manipulation/script.js
@@ -0,0 +1,13 @@
+/* 
+
+DOM - Document Object Model
+Structured representation of HTML Documnets. Allows JavaScript to access HTML and style to Manipulate them.
+
+For example, we will be able to change text, to change HTML attributes and also to change CSS styles from our JavaScript. So we can say that DOM is basically a connection point between HTML documents and JavaScript code.
+
+DOM always start with the document object.
+HTML usually has two child elements, head and body.
+
+DOM and DOM methods are actually part of something called the web APIs. So the web API APIs are like libraries that browsers implement and that we can access from our JavaScript code. And API stands for Application Programming Interface.
+
+*/
