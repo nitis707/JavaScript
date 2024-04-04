7ad2c73777194ad1833fef777323e5e33a80f9da
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-04-04 08:09:41 +0530

    Initial commit

---
+++ 05. JavaScript in the Browser - DOM and Events Fundamentals/82. PROJECT 3/scriptLecture.js
@@ -0,0 +1,9 @@
+// Seclecting elements
+const score0Ele = document.querySelector("#score--0");
+const score1Ele = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+// Starting conditions
+score0Ele.textContent = 0;
+score1Ele.textContent = 0;
+diceEl.classList.add("hidden");
