9d90464928aade6041cc3e358fedda0a6951ed98
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-04-04 08:34:14 +0530

    Initial commit

--- 05. JavaScript in the Browser - DOM and Events Fundamentals/83. Rolling the Dice/script.js
+++ 05. JavaScript in the Browser - DOM and Events Fundamentals/83. Rolling the Dice/script.js
@@ -1,20 +1,10 @@
-"use strict";
-
 // Seclecting elements
-const score0Ele = document.querySelector("#score--0");
-const score1Ele = document.getElementById("score--1");
 const current0Ele = document.getElementById("current--0");
 const current1Ele = document.getElementById("current--1");
 
-const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-
-// Starting conditions
-score0Ele.textContent = 0;
-score1Ele.textContent = 0;
-diceEl.classList.add("hidden");
 
 let currentScore = 0;
 
